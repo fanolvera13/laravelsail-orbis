8bbf2393095faeab19846402e98ff479293e7440
root <root@evelen.dominio>
2021-08-09 14:59:43 -0500

    primer commit

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class DashboardController extends Controller
+{
+    //
+    public function index(){
+        if(Auth::user()->hasRole('clientes')){
+            return view('dash.dashclientes');
+        }elseif(Auth::user()->hasRole('user')){
+            return view('dash.dashuser');
+        }elseif(Auth::user()->hasRole('administrator')){
+            return view('dash.dashadmin');
+        }else{
+            return view('dashboard');
+        }
+    }
+}
